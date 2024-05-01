b5ce16a33aae7348611a3462b50f6eb6d6b8b1c0
sreenumr <sreenu959@gmail.com>
2024-05-01 11:43:18 +0100

    get_common() modified to find all occurences of common

--- Crossword.py
+++ Crossword.py
@@ -1,5 +1,8 @@
 import random
 import numpy as np
+import logging
+
+logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(levelname)s - %(message)s')
 
 class Crossword:
     def __init__(self) -> None:
@@ -36,10 +39,11 @@ class Crossword:
 
     def get_common(self,word:str, new_word:str) -> list:
         common_indices = []
-        for index, char in enumerate(word):
+        for index1, char1 in enumerate(word):
+            for index2, char2 in enumerate(new_word):
             # Check if character is common and its index in string2
-            if char in new_word:
-                common_indices.append((index, new_word.index(char)))
+                if char1 == char2:
+                    common_indices.append((index1, index2))
         return common_indices
     
     def place_word(self,word:str) -> tuple:
@@ -48,7 +52,7 @@ class Crossword:
         # is_horizontal = True
         #First iteration when crossword is empty
         if(self.is_crossword_empty()):
-            print("Crossword is empty")
+            logging.info("Crossword is empty, adding word " + word)
             if(is_horizontal == False):
                 x,y = self.get_x_y(is_horizontal,len(word))
                 # print("Coordinates are " + str(x) + "," + str(y))             
@@ -62,7 +66,7 @@ class Crossword:
         
         #After adding first word to crossword
         else:
-            print("Adding next word " + word)
+            logging.info("Adding next word "+ word)
             if(self.check_intersection(word,is_horizontal)):
                 pass
             else:
@@ -74,13 +78,16 @@ class Crossword:
     def check_intersection(self,new_word:str,is_horizontal_new:bool) -> None:
         # words_filtered = filter(lambda word : word[2] == True) #Check direction, 2nd pos in tuple
         for word in self.placed_words:
-            if(word[2] == False and is_horizontal_new == True): #if cross with vertical word
+            if(word[2] == False and is_horizontal_new == True): #new word horizontal X vertical
                 cmn_idx = self.get_common(word[3],new_word)
                 # new_word_start = 
-                print(cmn_idx)
+                logging.info(f"Common Index = {str(cmn_idx)}")
                 pass
+            elif(word[2] == True and is_horizontal_new == False): #new word vertical X horizontal
+                cmn_idx = self.get_common(word[3],new_word)
 
-        print()
+                logging.info(f"Common Index = {str(cmn_idx)}")
+
         pass
 
     def generate_crossword(self) -> None:
@@ -88,11 +95,11 @@ class Crossword:
         self.grid_size = len(max(word_list, key=len))
         # added_words = []
         self.grid = [[0] * self.grid_size for _ in range(self.grid_size)]
-        # print(crossword)
+        # logging.info(crossword)
         for word in word_list:
             word_tup = self.place_word(word) #x,y,direction,word
             self.placed_words.append(word_tup)
-            word_list.pop(0)
-            # print(word_tup)
+            # word_list.pop(0)
+            # logging.info(word_tup)
             
-        print(np.matrix(self.grid))            
+        logging.info(np.matrix(self.grid))            
